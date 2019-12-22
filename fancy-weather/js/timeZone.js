bebca3293889e1ffdc9577c928cab25d4b16f01c
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2019-12-22 22:30:45 +0300

    Update timeZone.js

--- fancy-weather/js/timeZone.js
+++ fancy-weather/js/timeZone.js
@@ -1,6 +1,6 @@
 export default async function getTimeZone(){
     
-    const url = `http://api.timezonedb.com/v2.1/get-time-zone?key=WATWWREWYH81&format=json&by=position&lat=${localStorage['city-latitude']}&lng=${localStorage['city-longitude']}
+    const url = `https://api.timezonedb.com/v2.1/get-time-zone?key=WATWWREWYH81&format=json&by=position&lat=${localStorage['city-latitude']}&lng=${localStorage['city-longitude']}
     `;
     const response = await fetch(url);
     const data = await response.json();
