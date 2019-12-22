dac6f82c893b0fbd403e5e1e62e3a6c653c3f93b
denis-maslovskiy <53052374+denis-maslovskiy@users.noreply.github.com>
2019-12-22 22:17:27 +0300

    Update input.js

--- fancy-weather/js/input.js
+++ fancy-weather/js/input.js
@@ -1,4 +1,5 @@
 export default function getValueFromInput(){
     let value = document.querySelector('.input-and-search__input').value;
-    return value || localStorage['current-city'];
+//     return value || localStorage['current-city'];
+    return value || 'Minsk';
 }
